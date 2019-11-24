a734d0753f1a568c76f1b93bddddb5a3b5a59d13
Maksym Osadchuk <mosadchuk7@gmail.com>
2019-11-24 21:18:55 +0200

    Updates

---
+++ precache-manifest.ae70a7eaa93f7ba4b3c995edb3b71e35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41e2eeffe92ac6e8e5918067728168a",
+    "url": "/maximusII/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "068c102c63c39b9e06a7",
+    "url": "/maximusII/goit-react-hw-04-reader/static/css/main.7ead7c48.chunk.css"
+  },
+  {
+    "revision": "af3898b7702ff122c5b3",
+    "url": "/maximusII/goit-react-hw-04-reader/static/js/2.22bd2d44.chunk.js"
+  },
+  {
+    "revision": "068c102c63c39b9e06a7",
+    "url": "/maximusII/goit-react-hw-04-reader/static/js/main.d863a990.chunk.js"
+  },
+  {
+    "revision": "758037c86294bdcc879d",
+    "url": "/maximusII/goit-react-hw-04-reader/static/js/runtime-main.ec27852a.js"
+  }
+]);
